4b713c6813a13153b7a4b88cb051b78620dc9b34
Atlanta19 <shorty44.business@gmail.com>
2023-11-17 01:47:00 +0700

    Fix login

--- service/users/forms.py
+++ service/users/forms.py
@@ -1,24 +1,16 @@
 from django import forms
-from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import CustomUser
 
 
-class RegistrationUserForm(forms.ModelForm):
-    password2 = forms.CharField(label='Подтвердите пароль', widget=forms.PasswordInput,
-                                help_text='Введите пароль ещё раз')
-    consent = forms.BooleanField(label='Согласие на обработку данных', widget=forms.CheckboxInput,)
-
-    def clean(self):
-        super().clean()
-        password1 = self.cleaned_data['password']
-        password2 = self.cleaned_data['password2']
-        if password1 and password2 and password1 != password2:
-            errors = {'password2': ValidationError('Введённые пароли не совпадают', code='password_mismatch')}
-            raise ValidationError(errors)
-
-
+class RegistrationUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput)
+    email = forms.EmailField(label='Email', widget=forms.EmailInput)
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput)
+    consent = forms.BooleanField(label='Согласие на обработку данных', widget=forms.CheckboxInput, )
 
     class Meta:
         model = CustomUser
-        fields = ('full_name', 'username', 'email', 'password', 'password2', 'consent',)
+        fields = ('full_name', 'username', 'email', 'password1', 'password2', 'consent',)
